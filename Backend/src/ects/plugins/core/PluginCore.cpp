807b4d66ff7334eff2b2d11e217aee2573d89098
Péter Bohner (xzvf) <peter@bohner.me>
2023-07-24 16:52:16 +0200

    Implemented /ects/ects_status message

--- Backend/src/ects/plugins/core/PluginCore.cpp
+++ Backend/src/ects/plugins/core/PluginCore.cpp
@@ -1,13 +1,27 @@
 #include "PluginCore.hpp"
-#include "ects/ECTS.hpp"
 
 namespace ects::plugins::core {
 auto PluginCore::init(ects::ECTS *ects) -> void {
   ROS_INFO_STREAM("Initializing PluginCore");
-  m_timer = ects->m_timerManager->createTimer(
-      1.0, [this] { ROS_INFO("Timer callback"); });
-  ROS_INFO("Timer created");
+  // m_timer = ects->m_timerManager->createTimer(
+  //     1.0, [this] { ROS_INFO("Timer callback"); });
+  // ROS_INFO("Timer created");
+  m_ects = ects;
+  m_nodeHandle = ros::NodeHandle();
+  m_statusServer = m_nodeHandle.advertiseService(
+      "/ects/ects_status", &PluginCore::statusCallback, this);
 };
+
+auto PluginCore::statusCallback(ects::ECTSStatus::Request &req,
+                                ects::ECTSStatus::Response &res) -> bool {
+  ROS_INFO_STREAM("Status callback");
+  res.version = "0.0.1";
+  res.plugins_loaded = m_ects->m_config->get_value<std::vector<std::string>>(
+      "/core/load_plugins");
+  res.robot_name = m_ects->m_config->get_value<std::string>("/core/robot_name");
+  return true;
+}
+
 auto PluginCore::transmit_all() -> void {
   ROS_INFO_STREAM("Transmitting all");
 };
--- Backend/src/ects/plugins/core/PluginCore.hpp
+++ Backend/src/ects/plugins/core/PluginCore.hpp
@@ -4,15 +4,21 @@
 #include "ects/Timer.hpp"
 #include "ros/ros.h"
 
+#include "ects/ECTS.hpp"
+#include "ects/ECTSStatus.h"
 namespace ects::plugins::core {
 class PluginCore : public Plugin {
 public:
   auto init(ECTS *) -> void override;
   auto transmit_all() -> void override;
   auto transmit(std::string &topic_name) -> void override;
+  auto statusCallback(ects::ECTSStatus::Request &req,
+                      ects::ECTSStatus::Response &res) -> bool;
 
 private:
   std::shared_ptr<ects::Timer> m_timer;
-  // RosServer m_statusServer;
+  ros::NodeHandle m_nodeHandle;
+  ros::ServiceServer m_statusServer;
+  ECTS *m_ects;
 };
 } // namespace ects::plugins::core
