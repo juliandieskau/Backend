8d9ef539af082ade809ed08ea2059cfdb08c7d44
uztrd <uztrd@student.kit.edu>
2023-07-25 23:05:30 +0200

    add waypoint

---
+++ Backend/src/ects/plugins/waypoint/Waypoint.cpp
@@ -0,0 +1,38 @@
+#include "Waypoint.hpp"
+
+#include <utility>
+
+namespace ects::plugins::waypoints {
+
+Position2d Waypoint::get_position() { return position; }
+Waypoint Waypoint::from_ros(Waypoint::ros_t ros_input) {
+    auto wait = std::chrono::duration<double>(ros_input.wait_time);
+    std::optional<Heading2d> heading = std::nullopt;
+    if (ros_input.use_heading)
+        heading = {ros_input.pose.theta, ros_input.heading_accuracy};
+    auto pos = Position2d(ros_input.pose.x, ros_input.pose.y, ros_input.radius);
+
+    return {ros_input.name, pos, heading, wait};
+}
+Waypoint::ros_t Waypoint::to_ros(Waypoint wp) {
+    Waypoint::ros_t r;
+    auto heading = wp.heading.value_or(Heading2d{0, 0});
+    Waypoint::ros_t::_pose_type p;
+    p.x = wp.position.get_x();
+    p.y = wp.position.get_y();
+    p.theta = heading.get_heading();
+    r.pose = p;
+    r.radius = wp.position.get_radius();
+    r.use_heading = wp.heading.has_value();
+    r.heading_accuracy = wp.heading->get_accuracy();
+    r.wait_time = wp.wait_time.count();
+    r.name = wp.name;
+    return r;
+}
+Waypoint::Waypoint(std::string name, Position2d position,
+                   std::optional<Heading2d> heading,
+                   std::chrono::duration<double> wait_time)
+    : name(std::move(name)), position(position), heading(heading),
+      wait_time(wait_time) {}
+
+} // namespace ects::plugins::waypoints
---
+++ Backend/src/ects/plugins/waypoint/Waypoint.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "ects/Common.hpp"
+#include "ects/Waypoint.h"
+#include <chrono>
+
+namespace ects::plugins::waypoints {
+using common::Position2d, common::Heading2d;
+
+class Waypoint {
+  public:
+    using ros_t = ects::Waypoint;
+    using from_ros_t = ros_t;
+    using to_ros_t = ros_t;
+
+    Position2d get_position();
+    static Waypoint from_ros(ros_t);
+    static ros_t to_ros(Waypoint);
+
+  private:
+    Waypoint(std::string name, Position2d position,
+             std::optional<Heading2d> heading,
+             std::chrono::duration<double> wait_time);
+    std::string name;
+    Position2d position;
+    std::optional<Heading2d> heading;
+    std::chrono::duration<double> wait_time;
+};
+
+} // namespace ects::plugins::waypoints
