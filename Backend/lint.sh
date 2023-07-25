86f19ff01857261bfa3b9eb455f2351728aa18ba
Péter Bohner (xzvf) <peter@bohner.me>
2023-07-25 19:18:41 +0200

    Add .clang-format and linting script

---
+++ Backend/lint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+FILES="$(find src/ -name '*.hpp' -or -name '*.cpp' | grep -v nlohmann)"
+clang-format -i --sort-includes $FILES
+cpplint --verbose 5 --linelength 120 $FILES
