64fa50545cee17a0aff9fa60d9015e1f146fc9c6
uztrd <uztrd@student.kit.edu>
2023-07-25 16:20:51 +0200

    add publishing of battery messages on update or per transmit(_all)

--- Backend/src/ects/plugins/battery/battery_monitor.cpp
+++ Backend/src/ects/plugins/battery/battery_monitor.cpp
@@ -7,18 +7,53 @@ auto battery_monitor::init(ECTS *ects) -> void {
     data = {
             std::nullopt,
             ects->m_rosIf.create_subscriber<battery_state>(robot_battery_topic),
+            ects->m_rosIf.create_publisher<battery_state>(battery_state_topic),
+            ects->m_rosIf.create_publisher<charge_percentage>(percentage_topic),
+            ects->m_rosIf.create_publisher<estimated_runtime>(runtime_topic),
+            ects->m_rosIf.create_publisher<warning>(warning_topic),
     };
-    data->battery_subscriber.subscribe([](battery_state state) {
-        ROS_INFO("battery update");
+    data->battery_subscriber.subscribe([this](battery_state state) {
+        data->state = state;
+        transmit_all();
     });
 }
 
 auto battery_monitor::transmit_all() -> void {
-
+    if (data->state.has_value()) {
+        publish_battery_state();
+        publish_percentage();
+        publish_runtime();
+        publish_warning();
+    }
 }
 
 auto battery_monitor::transmit(std::string &topic_name) -> void {
+    if (data->state.has_value()) {
+        if (topic_name == battery_state_topic)
+            publish_battery_state();
+        if (topic_name == percentage_topic)
+            publish_percentage();
+        if (topic_name == runtime_topic)
+            publish_runtime();
+        if (topic_name == warning_topic)
+            publish_warning();
+    }
+}
 
+auto battery_monitor::publish_battery_state() -> void {
+    data->battery_state_publisher.publish(*data->state);
+}
+
+auto battery_monitor::publish_percentage() -> void {
+    data->percentage_publisher.publish(data->state->get_charge());
+}
+
+auto battery_monitor::publish_runtime() -> void {
+    data->runtime_publisher.publish(data->state->get_estimated_runtime());
+}
+
+auto battery_monitor::publish_warning() -> void {
+    data->warning_publisher.publish(data->state->is_critical());
 }
 
 }
--- Backend/src/ects/plugins/battery/battery_monitor.hpp
+++ Backend/src/ects/plugins/battery/battery_monitor.hpp
@@ -12,11 +12,24 @@ public:
     auto transmit(std::string &topic_name) -> void override;
 
 private:
+    auto publish_battery_state() -> void;
+    auto publish_percentage() -> void;
+    auto publish_runtime() -> void;
+    auto publish_warning() -> void;
+
     struct data {
         std::optional<battery_state> state;
         subscriber<battery_state> battery_subscriber;
+        publisher<battery_state> battery_state_publisher;
+        publisher<charge_percentage> percentage_publisher;
+        publisher<estimated_runtime> runtime_publisher;
+        publisher<warning> warning_publisher;
     };
     std::optional<data> data;
+    static constexpr auto battery_state_topic = "/ects/battery/usage";
+    static constexpr auto percentage_topic = "/etcs/battery/percentage";
+    static constexpr auto runtime_topic = "/ects/battery/estimated_time_remaining";
+    static constexpr auto warning_topic = "/etcs/battery/is_critical";
 };
 
 }
