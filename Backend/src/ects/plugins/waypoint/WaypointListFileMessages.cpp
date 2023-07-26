cc8ffcd534b98e9fb4e294dc1fd2c1063593c91f
uztrd <uztrd@student.kit.edu>
2023-07-26 19:33:13 +0200

    add waypoint list file messages

---
+++ Backend/src/ects/plugins/waypoint/WaypointListFileMessages.cpp
@@ -0,0 +1,36 @@
+#include "WaypointListFileMessages.hpp"
+
+#include <utility>
+
+namespace ects::plugins::waypoints {
+
+FileListResponse::FileListResponse(std::vector<std::string> files)
+    : files(std::move(files)), error(std::nullopt) {}
+FileListResponse::FileListResponse(std::string error_message)
+    : files(), error(error_message) {}
+auto FileListResponse::to_ros(const FileListResponse &internal_input)
+    -> FileListResponse::ros_t {
+    FileListResponse::ros_t r{};
+    r.success = !internal_input.error.has_value();
+    r.error_message = internal_input.error.value_or("");
+    r.filenames = internal_input.files;
+    return r;
+}
+
+auto FileRequest::get_filename() -> std::string { return filename; }
+auto FileRequest::from_ros(const FileRequest::ros_t &ros_input) -> FileRequest {
+    return {ros_input.filename};
+}
+FileRequest::FileRequest(std::string filename)
+    : filename(std::move(filename)) {}
+
+FileResponse::FileResponse() : error(std::nullopt) {}
+FileResponse::FileResponse(std::string error_message) : error(error_message) {}
+auto FileResponse::to_ros(const FileResponse &internal_input)
+    -> FileResponse::ros_t {
+    FileResponse::ros_t r{};
+    r.success = !internal_input.error.has_value();
+    r.error_message = internal_input.error.value_or("");
+    return r;
+}
+} // namespace ects::plugins::waypoints
---
+++ Backend/src/ects/plugins/waypoint/WaypointListFileMessages.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "WaypointList.hpp"
+#include "ects/RosInterface.hpp"
+
+#include "ects/EmptyMessage.hpp"
+#include "ects/WaypointListDirectory.h"
+#include "ects/WaypointListFile.h"
+
+#include <string>
+#include <vector>
+
+namespace ects::plugins::waypoints {
+
+using file_list_service_ros_t = WaypointListDirectory;
+struct FileListService {};
+using FileListRequest = EmptyMessage<file_list_service_ros_t::Request>;
+class FileListResponse {
+  public:
+    using ros_t = file_list_service_ros_t::Response;
+    using to_ros_t = ros_t;
+
+    FileListResponse(std::vector<std::string> files);
+    FileListResponse(std::string error_message);
+    static auto to_ros(const FileListResponse &) -> ros_t;
+
+  private:
+    std::vector<std::string> files;
+    std::optional<std::string> error;
+};
+
+using waypoint_list_file_service_ros_t = WaypointListFile;
+struct WaypointListFileService {};
+class FileRequest {
+  public:
+    using ros_t = waypoint_list_file_service_ros_t::Request;
+    using from_ros_t = ros_t;
+
+    auto get_filename() -> std::string;
+    static auto from_ros(const ros_t &) -> FileRequest;
+
+  private:
+    FileRequest(std::string filename);
+
+    std::string filename;
+};
+class FileResponse {
+  public:
+    using ros_t = waypoint_list_file_service_ros_t::Response;
+    using to_ros_t = ros_t;
+
+    FileResponse();
+    FileResponse(std::string error_message);
+    static auto to_ros(const FileResponse &) -> ros_t;
+
+  private:
+    std::optional<std::string> error;
+};
+
+} // namespace ects::plugins::waypoints
+
+namespace ects {
+using namespace ects::plugins::waypoints;
+
+template <> struct server_traits<FileListService> {
+    using ros_t = file_list_service_ros_t;
+    using request_from_ros_t = FileListRequest;
+    using response_to_ros_t = FileListResponse;
+};
+
+template <> struct server_traits<WaypointListFileService> {
+    using ros_t = waypoint_list_file_service_ros_t;
+    using request_from_ros_t = FileRequest;
+    using response_to_ros_t = FileResponse;
+};
+
+} // namespace ects
