17b0494d4460ed9ff7336dbab429b6e0c12dabe0
Péter Bohner (xzvf) <peter@bohner.me>
2023-07-26 02:49:49 +0200

    Refactor PluginLoader::load error handling

--- Backend/src/ects/src/PluginLoader.cpp
+++ Backend/src/ects/src/PluginLoader.cpp
@@ -9,14 +9,10 @@ typedef Plugin *(*plugin_creator)();
 auto PluginLoader::load(std::string &plugin_name) -> Plugin * {
     auto plugin_so_name = "libects_plugin_" + plugin_name + ".so";
     void *so = dlopen(plugin_so_name.c_str(), RTLD_NOW);
-    if (so == nullptr) {
-        ROS_ERROR_STREAM("Could not load plugin " << plugin_name << ": "
-                                                  << dlerror());
-        return nullptr;
-    }
-    plugin_creator create_plugin =
-        (plugin_creator)dlsym(so, "create_plugin_instance");
-    if (create_plugin == nullptr) {
+    plugin_creator create_plugin = nullptr;
+    if (so != nullptr)
+        create_plugin = (plugin_creator)dlsym(so, "create_plugin_instance");
+    if (so == nullptr || create_plugin == nullptr) {
         ROS_ERROR_STREAM("Could not load plugin " << plugin_name << ": "
                                                   << dlerror());
         return nullptr;
