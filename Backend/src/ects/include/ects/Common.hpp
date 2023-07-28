ddb3f0c6d489d8948f4dec4f4cf395f555ff8672
uztrd <uztrd@student.kit.edu>
2023-07-28 18:46:45 +0200

    added 3d vector and twist

--- Backend/src/ects/include/ects/Common.hpp
+++ Backend/src/ects/include/ects/Common.hpp
@@ -2,6 +2,8 @@
 
 #include "nlohmann/json.hpp"
 #include <geometry_msgs/Pose2D.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Vector3.h>
 
 namespace ects::common {
 
@@ -31,6 +33,39 @@ class Heading2d {
   private:
     double heading;
     double accuracy;
+};
+
+class Vector3D {
+  public:
+    using ros_t = geometry_msgs::Vector3;
+    using from_ros_t = ros_t;
+    using to_ros_t = ros_t;
+
+    static auto from_ros(const ros_t &) -> Vector3D;
+    static auto to_ros(const Vector3D &) -> ros_t;
+
+  private:
+    Vector3D(double x, double y, double z);
+
+    double x;
+    double y;
+    double z;
+};
+
+class Twist {
+  public:
+    using ros_t = geometry_msgs::Twist;
+    using from_ros_t = ros_t;
+    using to_ros_t = ros_t;
+
+    static auto from_ros(const ros_t &) -> Twist;
+    static auto to_ros(const Twist &) -> ros_t;
+
+  private:
+    Twist(Vector3D linear, Vector3D angular);
+
+    Vector3D linear;
+    Vector3D angular;
 };
 
 } // namespace ects::common
--- Backend/src/ects/src/Common.cpp
+++ Backend/src/ects/src/Common.cpp
@@ -13,4 +13,25 @@ Heading2d::Heading2d(double heading, double accuracy)
 double Heading2d::get_heading() const { return heading; }
 double Heading2d::get_accuracy() const { return accuracy; }
 
+auto Twist::from_ros(const Twist::ros_t &ros) -> Twist {
+    return {Vector3D::from_ros(ros.linear), Vector3D::from_ros(ros.angular)};
+}
+auto Twist::to_ros(const Twist &tw) -> Twist::ros_t {
+    Twist::ros_t r;
+    r.linear = Vector3D::to_ros(tw.linear);
+    r.angular = Vector3D::to_ros(tw.angular);
+    return r;
+}
+Twist::Twist(Vector3D linear, Vector3D angular)
+    : linear(linear), angular(angular) {}
+
+auto Vector3D::from_ros(const Vector3D::ros_t &r) -> Vector3D {
+    return {r.x, r.y, r.z};
+}
+auto Vector3D::to_ros(const Vector3D &v) -> Vector3D::ros_t {
+    Vector3D::ros_t r;
+    std::tie(r.x, r.y, r.z) = std::tuple{v.x, v.y, v.z};
+    return r;
+}
+Vector3D::Vector3D(double x, double y, double z) : x(x), y(y), z(z) {}
 } // namespace ects::common
