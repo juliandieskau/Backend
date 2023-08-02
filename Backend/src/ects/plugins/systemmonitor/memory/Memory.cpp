8082e3c6adff4e44f6fed5834083a67db31b8c0a
Péter Bohner (xzvf) <peter@bohner.me>
2023-08-02 20:04:48 +0200

    SystemMonitor: Implement Memory usage collection

--- Backend/src/ects/plugins/systemmonitor/memory/Memory.cpp
+++ Backend/src/ects/plugins/systemmonitor/memory/Memory.cpp
@@ -1,1 +1,19 @@
 #include "Memory.hpp"
+
+#include <sys/sysinfo.h>
+
+namespace ects::plugins::systemmonitor {
+auto Memory::get_usage() -> MemoryUsageMessage {
+    struct sysinfo info;
+    sysinfo(&info);
+    return MemoryUsageMessage(info.totalram * info.mem_unit,
+                              (info.totalram - info.freeram) * info.mem_unit,
+                              info.freeram * info.mem_unit,
+                              info.sharedram * info.mem_unit,
+                              info.bufferram * info.mem_unit,
+                              (info.freeram + info.bufferram) * info.mem_unit,
+                              info.totalswap * info.mem_unit,
+                              (info.totalswap - info.freeswap) * info.mem_unit,
+                              info.freeswap * info.mem_unit);
+}
+} // namespace ects::plugins::systemmonitor
