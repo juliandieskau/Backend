a158283fe9867bb90ee446d8f4622a57b9a97627
Péter Bohner (xzvf) <peter@bohner.me>
2023-08-02 20:07:56 +0200

    SystemMonitor: implement ProcessTotal usage collection

--- Backend/src/ects/plugins/systemmonitor/programs/Programs.cpp
+++ Backend/src/ects/plugins/systemmonitor/programs/Programs.cpp
@@ -1,1 +1,12 @@
 #include "Programs.hpp"
+#include <sys/sysinfo.h>
+
+namespace ects::plugins::systemmonitor {
+
+auto Programs::get_usage() -> ProcessTotalMessage {
+    struct sysinfo info;
+    sysinfo(&info);
+    return ProcessTotalMessage(info.procs);
+}
+
+} // namespace ects::plugins::systemmonitor
