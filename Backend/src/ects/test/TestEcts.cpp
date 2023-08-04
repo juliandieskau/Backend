2909031124e536ef29e9be7eb9ba11f96a32c5ae
uztrd <uztrd@student.kit.edu>
2023-08-04 20:47:07 +0200

    expanded the config test to include the get_value_or_default method

--- Backend/src/ects/test/TestEcts.cpp
+++ Backend/src/ects/test/TestEcts.cpp
@@ -19,6 +19,14 @@ TEST(EctsCore, loadConfig) {
     auto config = ects::Configuration::load_configuration(configFilePath);
     ASSERT_TRUE(config.has_value());
     ASSERT_EQ(config->get_value<std::string>("/core/robot_name"), "test_robot");
+    ASSERT_EQ(config->get_value_or_default<std::string>("/core/robot_name",
+                                                        "default"),
+              "test_robot");
+    ASSERT_ANY_THROW(config->get_value<int>("/core/robot_name"))
+        << "throw on wrong type";
+    ASSERT_ANY_THROW(config->get_value<int>("/nonexistent"))
+        << "throw on nonexistent key";
+    ASSERT_EQ(config->get_value_or_default<int>("/nonexistent", -11), -11);
 }
 
 int main(int argc, char **argv) {
