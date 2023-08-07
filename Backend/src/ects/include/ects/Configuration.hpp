b1b5a469a29322de01b323dd0e5820e313a6c948
uztrd <uztrd@student.kit.edu>
2023-08-07 15:51:56 +0200

    log defaulting a config value

--- Backend/src/ects/include/ects/Configuration.hpp
+++ Backend/src/ects/include/ects/Configuration.hpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <iostream>
 #include <optional>
+#include <utility>
 
 using json = nlohmann::json;
 
@@ -14,25 +15,27 @@ class Configuration {
     static auto load_configuration(std::string path)
         -> std::optional<Configuration>;
 
-    template <typename T> auto get_value(std::string key) const -> T {
+    template <typename T> auto get_value(const std::string &key) const -> T {
         return config_json.at(json::json_pointer(key)).get<T>();
     }
     template <typename T>
-    auto get_value_or_default(std::string key, T default_value) const -> T {
+    auto get_value_or_default(const std::string &key,
+                              const T &default_value) const -> T {
         try {
             auto x = config_json.at(json::json_pointer(key));
-            if (x.is_null())
-                return default_value;
-            return x.get<T>();
+            if (!x.is_null())
+                return x.get<T>();
         } catch (json::exception &e) {
-            return default_value;
         }
+        ROS_INFO_STREAM("default config at " << key << " to " << default_value);
+        return default_value;
     }
 
     auto dump() const -> std::string { return config_json.dump(); }
 
   private:
-    explicit Configuration(json config_json) : config_json(config_json) {}
+    explicit Configuration(json config_json)
+        : config_json(std::move(config_json)) {}
 
     json config_json;
 };
