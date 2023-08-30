c3e37816927c2edec362bc36f3175d42bf8d2276
Péter Bohner (xzvf) <peter@bohner.me>
2023-08-30 17:10:06 +0200

    Finished adding unit tests

--- Backend/src/ects/test/SystemMonitorTest.cpp
+++ Backend/src/ects/test/SystemMonitorTest.cpp
@@ -96,7 +96,8 @@ TEST(SystemMonitor, cpu) {
     ASSERT_NO_THROW(cpu.get_usage());
     ros::WallDuration(0.1).sleep(); // cpu needs two samples to calculate usage
     auto usage = cpu.get_usage();
-    auto CpuUsage = ects::plugins::systemmonitor::CpuUsageMessage::to_ros(usage);
+    auto CpuUsage =
+        ects::plugins::systemmonitor::CpuUsageMessage::to_ros(usage);
     ASSERT_GE(CpuUsage.total_usage, 0.0);
     ASSERT_GT(CpuUsage.per_core_usage.size(), 0);
     for (auto &core : CpuUsage.per_core_usage) {
@@ -162,7 +163,12 @@ static const auto systemmonitor_config =
     "}\n";
 
 static bool recv_cpu = false, recv_cpu_agg = false, recv_agg = false;
-TEST(EctsPlugins, systemmonitor_cpu) {
+static bool recv_mem = false, recv_mem_agg = false;
+static bool recv_mp = false;
+static bool recv_adapters = false, recv_adapters_info = false,
+            recv_adapter_usage = false, recv_adapters_agg = false;
+static bool recv_programs = false, recv_programs_agg = false;
+TEST(EctsPlugins, systemmonitor) {
     auto ects = ects_with_config(systemmonitor_config);
     load_test_plugin(ects, "systemmonitor");
     ros::NodeHandle nh;
@@ -178,14 +184,12 @@ TEST(EctsPlugins, systemmonitor_cpu) {
         auto cpu_sub = nh.subscribe<ects::CpuUsage>(
             "/ects/system/cpu/usage", 0,
             [&](const ects::CpuUsage::ConstPtr &msg) {
-                ROS_INFO("got cpu");
                 EXPECT_GT(msg->total_usage, 0.0);
                 recv_cpu = true;
             });
         auto cpu_agg_sub = nh.subscribe<ects::CpuUsageHistory>(
             "/ects/system/averages/ta/cpu/usage", 0,
             [&](const ects::CpuUsageHistory::ConstPtr &msg) {
-                ROS_INFO("got cpu agg");
                 if (!msg->measurements.empty()) {
                     EXPECT_GT(msg->measurements[0].total_usage, 0.0);
                 }
@@ -201,23 +205,21 @@ TEST(EctsPlugins, systemmonitor_cpu) {
             recv_cpu_agg = false;
         };
         reset_recv();
-        spin_predicate(recv_all_cpu, 50);
+        spin_predicate(recv_all_cpu, 10);
         EXPECT_FALSE(recv_all_cpu());
 
         reset_recv();
-        ROS_INFO("retransmitting");
         retransmit_topic("/ects/system/cpu/usage");
         retransmit_topic("/ects/system/averages/ta/cpu/usage");
-        spin_predicate(recv_all_cpu, 50);
+        spin_predicate(recv_all_cpu, 100);
         EXPECT_TRUE(recv_all_cpu());
 
         reset_recv();
         retransmit_topic("");
-        spin_predicate(recv_all_cpu, 50);
+        spin_predicate(recv_all_cpu, 100);
         EXPECT_TRUE(recv_all_cpu());
     }
     {
-        volatile bool recv_mp = false;
         std::vector<std::string> rosnames;
         std::thread mountpoints([&]() {
             ros::NodeHandle nh;
@@ -239,7 +241,6 @@ TEST(EctsPlugins, systemmonitor_cpu) {
             auto disk_sub = nh.subscribe<ects::DiskUsage>(
                 "/ects/system/disk/" + rn + "/usage", 0,
                 [&](const ects::DiskUsage::ConstPtr &msg) {
-                    ROS_INFO_STREAM("got disk: " << rn);
                     EXPECT_GT(msg->size_total, 0);
                     EXPECT_GT(msg->used, 0);
                     recv_cpu = true;
@@ -247,7 +248,6 @@ TEST(EctsPlugins, systemmonitor_cpu) {
             auto disk_agg_sub = nh.subscribe<ects::DiskUsageHistory>(
                 "/ects/system/averages/ta/disk/" + rn + "/usage", 0,
                 [&](const ects::DiskUsageHistory::ConstPtr &msg) {
-                    ROS_INFO_STREAM("got disk agg: " << rn);
                     if (!msg->measurements.empty()) {
                         EXPECT_GT(msg->measurements[0].size_total, 0);
                         EXPECT_GT(msg->measurements[0].used, 0);
@@ -265,14 +265,168 @@ TEST(EctsPlugins, systemmonitor_cpu) {
             reset_recv();
             retransmit_topic("/ects/system/disk/" + rn + "/usage");
             retransmit_topic("/ects/system/averages/ta/disk/" + rn + "/usage");
-            spin_predicate(recv_all_disk, 50);
+            spin_predicate(recv_all_disk, 100);
             EXPECT_TRUE(recv_all_disk());
 
             reset_recv();
             retransmit_topic("");
-            spin_predicate(recv_all_disk, 50);
+            spin_predicate(recv_all_disk, 100);
             EXPECT_TRUE(recv_all_disk());
         }
+    }
+    {
+
+        auto memory_sub = nh.subscribe<ects::MemoryUsage>(
+            "/ects/system/mem/usage", 0,
+            [&](const ects::MemoryUsage::ConstPtr &msg) {
+                EXPECT_GT(msg->total, 0);
+                EXPECT_GT(msg->used, 0);
+                EXPECT_GE(msg->free, 0);
+                EXPECT_GE(msg->shared, 0);
+                EXPECT_GE(msg->buff_cache, 0);
+                EXPECT_GE(msg->available, 0);
+                EXPECT_GE(msg->swap_total, 0);
+                EXPECT_GE(msg->swap_used, 0);
+                EXPECT_GE(msg->swap_free, 0);
+                recv_mem = true;
+            });
+        auto memory_agg_sub = nh.subscribe<ects::MemoryUsageHistory>(
+            "/ects/system/averages/ta/mem/usage", 0,
+            [&](const ects::MemoryUsageHistory::ConstPtr &msg) {
+                if (!msg->measurements.empty()) {
+                    EXPECT_GT(msg->measurements[0].total, 0);
+                    EXPECT_GT(msg->measurements[0].used, 0);
+                    EXPECT_GE(msg->measurements[0].free, 0);
+                    EXPECT_GE(msg->measurements[0].shared, 0);
+                    EXPECT_GE(msg->measurements[0].buff_cache, 0);
+                    EXPECT_GE(msg->measurements[0].available, 0);
+                    EXPECT_GE(msg->measurements[0].swap_total, 0);
+                    EXPECT_GE(msg->measurements[0].swap_used, 0);
+                    EXPECT_GE(msg->measurements[0].swap_free, 0);
+                }
+                EXPECT_EQ(msg->aggregation.ectsname, "ta");
+                recv_mem_agg = true;
+            });
+        auto recv_all_mem = [&]() { return recv_mem && recv_mem_agg; };
+        spin_predicate(recv_all_mem, 1000);
+        EXPECT_TRUE(recv_all_mem());
+        auto reset_recv = [&]() {
+            recv_mem = false;
+            recv_mem_agg = false;
+        };
+        reset_recv();
+        retransmit_topic("/ects/system/mem/usage");
+        retransmit_topic("/ects/system/averages/ta/mem/usage");
+        spin_predicate(recv_all_mem, 100);
+        EXPECT_TRUE(recv_all_mem());
+
+        reset_recv();
+        retransmit_topic("");
+        spin_predicate(recv_all_mem, 100);
+        EXPECT_TRUE(recv_all_mem());
+    }
+    {
+        std::vector<std::string> adapter_names;
+        std::thread adapter_thread([&]() {
+            ros::NodeHandle nh;
+            auto adapter_caller = nh.serviceClient<ects::AdapterList>(
+                "/ects/system/network/adapters");
+            auto adapters = ects::AdapterList();
+            ASSERT_TRUE(adapter_caller.call(adapters));
+            ASSERT_FALSE(adapters.response.adapters.empty());
+            recv_adapters = true;
+            adapter_names = std::vector(adapters.response.adapters.begin(),
+                                        adapters.response.adapters.end());
+        });
+        spin_predicate([&]() { return recv_adapters; }, 1000);
+        adapter_thread.join();
+        EXPECT_TRUE(recv_adapters);
+
+        for (auto adapter : adapter_names) {
+            auto info_sub = nh.subscribe<ects::NetworkInfo>(
+                "/ects/system/network/" + adapter + "/info", 0,
+                [&](const ects::NetworkInfo::ConstPtr &msg) {
+                    EXPECT_EQ(msg->interface_name, adapter);
+                    recv_adapters_info = true;
+                });
+            auto usage_sub = nh.subscribe<ects::NetworkUsage>(
+                "/ects/system/network/" + adapter + "/usage", 0,
+                [&](const ects::NetworkUsage::ConstPtr &msg) {
+                    EXPECT_GE(msg->wifi_signal_strength, 0);
+                    // NOTE: we can't really test down and up speed here
+                    recv_adapter_usage = true;
+                });
+            auto adapter_agg_sub = nh.subscribe<ects::NetworkUsageHistory>(
+                "/ects/system/averages/ta/network/" + adapter + "/usage", 0,
+                [&](const ects::NetworkUsageHistory::ConstPtr &msg) {
+                    if (!msg->measurements.empty()) {
+                        EXPECT_GE(msg->measurements[0].wifi_signal_strength, 0);
+                    }
+                    EXPECT_EQ(msg->aggregation.ectsname, "ta");
+                    recv_adapters_agg = true;
+                });
+            auto recv_all_net = [&]() {
+                return recv_adapters_info && recv_adapter_usage &&
+                       recv_adapters_agg;
+            };
+            spin_predicate(recv_all_net, 1000);
+            EXPECT_TRUE(recv_all_net());
+
+            auto reset_recv = [&]() {
+                recv_adapters_info = false;
+                recv_adapter_usage = false;
+                recv_adapters_agg = false;
+            };
+            reset_recv();
+            retransmit_topic("/ects/system/network/" + adapter + "/info");
+            retransmit_topic("/ects/system/network/" + adapter + "/usage");
+            retransmit_topic("/ects/system/averages/ta/network/" + adapter +
+                             "/usage");
+            spin_predicate(recv_all_net, 100);
+            EXPECT_TRUE(recv_all_net());
+
+            reset_recv();
+            retransmit_topic("");
+            spin_predicate(recv_all_net, 100);
+            EXPECT_TRUE(recv_all_net());
+        }
+    }
+    {
+        auto programs_sub = nh.subscribe<ects::ProcessTotal>(
+            "/ects/system/processes/total", 0,
+            [&](const ects::ProcessTotal::ConstPtr &msg) {
+                EXPECT_GT(msg->number_of_processes, 1);
+                recv_programs = true;
+            });
+        auto programs_agg_sub = nh.subscribe<ects::ProcessTotalHistory>(
+            "/ects/system/averages/ta/processes/total", 0,
+            [&](const ects::ProcessTotalHistory::ConstPtr &msg) {
+                if (!msg->measurements.empty()) {
+                    EXPECT_GT(msg->measurements[0].number_of_processes, 1);
+                }
+                EXPECT_EQ(msg->aggregation.ectsname, "ta");
+                recv_programs_agg = true;
+            });
+
+        auto recv_all_programs = [&]() {
+            return recv_programs && recv_programs_agg;
+        };
+        spin_predicate(recv_all_programs, 1000);
+
+        auto reset_recv = [&]() {
+            recv_programs = false;
+            recv_programs_agg = false;
+        };
+        reset_recv();
+        retransmit_topic("/ects/system/processes/total");
+        retransmit_topic("/ects/system/averages/ta/processes/total");
+        spin_predicate(recv_all_programs, 100);
+        EXPECT_TRUE(recv_all_programs());
+
+        reset_recv();
+        retransmit_topic("");
+        spin_predicate(recv_all_programs, 100);
+        EXPECT_TRUE(recv_all_programs());
     }
 
     {
